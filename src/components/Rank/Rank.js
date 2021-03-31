81a098f9145bcaafa8d5fb095080dcab928ac237
fredrikgerner <fred.gerner@icloud.com>
2021-03-31 19:59:22 +1100

    added form and background styles

---
+++ src/components/Rank/Rank.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './Rank.css'
+
+
+const Rank = () => {
+    return (
+        <div>
+            <div className={'white f3'}>
+                {'Fred, your current rank is...'}
+            </div>
+            <div className={'white f1'}>
+                {'#3'}
+            </div>
+        </div>
+    )
+}
+
+
+export default Rank;
